a9bef2eb877085a5a4ef348c136c4aba33268f28
ytgw <tagawa0920@gmail.com>
2023-03-20 02:23:50 +0900

    リクエストボディがある場合のテストを追加。

--- main_test.go
+++ main_test.go
@@ -40,3 +40,25 @@ func TestGetHandler(t *testing.T) {
 	assertContainsBody(t, responseRecorder.Body.String(), "<h2>Request Body</h2>")
 	assertContainsBody(t, responseRecorder.Body.String(), "Empty request body.")
 }
+
+func TestPostHandler(t *testing.T) {
+	// setup
+	request, err := http.NewRequest("GET", "http://example.com/", strings.NewReader("post data"))
+	if err != nil {
+		t.Fatal(err)
+	}
+	request.Header.Add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8")
+	responseRecorder := httptest.NewRecorder()
+	testHandler := http.HandlerFunc(handler)
+
+	// act
+	testHandler.ServeHTTP(responseRecorder, request)
+
+	// check
+	assertIsStatusOK(t, responseRecorder.Code)
+	assertContainsBody(t, responseRecorder.Body.String(), "<h2>Basic Request Information</h2>")
+	assertContainsBody(t, responseRecorder.Body.String(), "<tr><td>Method:</td><td>GET</td></tr>")
+	assertContainsBody(t, responseRecorder.Body.String(), "<h2>Other Request Headers</h2>")
+	assertContainsBody(t, responseRecorder.Body.String(), "<h2>Request Body</h2>")
+	assertContainsBody(t, responseRecorder.Body.String(), "post data")
+}
