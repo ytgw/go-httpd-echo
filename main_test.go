cc0a81e993cd7d87eec9d127b6b69b95d260ce64
ytgw <tagawa0920@gmail.com>
2023-03-20 02:32:47 +0900

    TCP Exposerを経由を模擬した場合のテスト。

--- main_test.go
+++ main_test.go
@@ -62,3 +62,32 @@ func TestPostHandler(t *testing.T) {
 	assertContainsBody(t, responseRecorder.Body.String(), "<h2>Request Body</h2>")
 	assertContainsBody(t, responseRecorder.Body.String(), "post data")
 }
+
+func TestTCPExposerHandler(t *testing.T) {
+	// setup
+	request, err := http.NewRequest("GET", "http://echo.tcpexposer.com/", strings.NewReader(""))
+	if err != nil {
+		t.Fatal(err)
+	}
+	request.Header.Add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8")
+	request.Header.Add("X-Forwarded-Proto", "http")
+	responseRecorder := httptest.NewRecorder()
+	testHandler := http.HandlerFunc(handler)
+
+	// act
+	testHandler.ServeHTTP(responseRecorder, request)
+
+	// check
+	assertIsStatusOK(t, responseRecorder.Code)
+	assertContainsBody(t, responseRecorder.Body.String(), "<h2>Basic Request Information</h2>")
+	assertContainsBody(t, responseRecorder.Body.String(), "<tr><td>Method:</td><td>GET</td></tr>")
+
+	assertContainsBody(t, responseRecorder.Body.String(), "<h2>Request Headers Add by TCP Exposer</h2>")
+	assertContainsBody(t, responseRecorder.Body.String(), "<tr><td>X-Forwarded-Proto:</td><td>http</td></tr>")
+
+	assertContainsBody(t, responseRecorder.Body.String(), "<h2>Other Request Headers</h2>")
+	assertContainsBody(t, responseRecorder.Body.String(), "<tr><td>Accept:</td><td>text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8</td></tr>")
+
+	assertContainsBody(t, responseRecorder.Body.String(), "<h2>Request Body</h2>")
+	assertContainsBody(t, responseRecorder.Body.String(), "Empty request body.")
+}
