352d9c4aeb960cbcdd292656ee10b08cdc847735
ytgw <tagawa0920@gmail.com>
2023-03-19 14:18:52 +0900

    HTMLでリスポンス。

--- main.go
+++ main.go
@@ -8,10 +8,19 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "<!DOCTYPE html>")
+	fmt.Fprintln(w, "<html>")
+	fmt.Fprintln(w, "<head><title>Your HTTP Request Information</title></head>")
+	fmt.Fprintln(w, "<body>")
 	fmt.Fprintln(w, html.EscapeString(r.Method))
+	fmt.Fprintln(w, "<br>")
 	fmt.Fprintln(w, html.EscapeString(r.URL.String()))
+	fmt.Fprintln(w, "<br>")
 	fmt.Fprintln(w, html.EscapeString(r.Proto))
+	fmt.Fprintln(w, "<br>")
 	fmt.Fprintln(w, html.EscapeString(r.Host))
+	fmt.Fprintln(w, "</body>")
+	fmt.Fprintln(w, "</html>")
 }
 
 func main() {
