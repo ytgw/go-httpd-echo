8587acd20b8cf5e3fb22a2e3c24e570c5872c282
ytgw <tagawa0920@gmail.com>
2023-03-19 14:14:03 +0900

    XSS対策のためのエスケープ処理を追加。

--- main.go
+++ main.go
@@ -2,15 +2,16 @@ package main
 
 import (
 	"fmt"
+	"html"
 	"log"
 	"net/http"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, r.Method)
-	fmt.Fprintln(w, r.URL.String())
-	fmt.Fprintln(w, r.Proto)
-	fmt.Fprintln(w, r.Host)
+	fmt.Fprintln(w, html.EscapeString(r.Method))
+	fmt.Fprintln(w, html.EscapeString(r.URL.String()))
+	fmt.Fprintln(w, html.EscapeString(r.Proto))
+	fmt.Fprintln(w, html.EscapeString(r.Host))
 }
 
 func main() {
